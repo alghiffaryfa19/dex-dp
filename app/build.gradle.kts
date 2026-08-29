e96e84e9ef0f203c72ad323c01216bf6c4523409
Fauzan <alghiffaryfa19@gmail.com>
2026-08-30 02:15:38 +0700

    fix: revert targetSdk to 30 to prevent FGS crash

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.fauzan.nativedex"
         minSdk = 30
-        targetSdk = 34
+        targetSdk = 30
         versionCode = 1
         versionName = "1.0"
 
